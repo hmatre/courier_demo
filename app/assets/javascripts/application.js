0c682704aaf5108d92e6d78fcce19c0b8335ddd9
Hitesh <hiteshmatre@gmail.com>
2021-07-10 00:45:49 +0530

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require bootstrap/dropdown
+//= require turbolinks
+
+//= require_tree .
+
+
+//= require bootstrap
---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_07_09_160235) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "sender_name"
+    t.string "sender_address"
+    t.string "sender_mobile_number"
+    t.string "sender_pincode"
+    t.string "receiver_name"
+    t.string "receiver_address"
+    t.string "receiver_mobile_number"
+    t.string "receiver_pincode"
+    t.string "parcel_weight"
+    t.integer "service_type"
+    t.float "service_charges"
+    t.integer "payment_mode"
+    t.integer "status"
+    t.integer "user_id"
+    t.string "tracking_reference_number"
+    t.string "sender_email"
+    t.string "receiver_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "phone_number"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
